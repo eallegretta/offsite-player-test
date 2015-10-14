c64abec169332d19cf14294695ae61023417b9ba
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2015-10-14 17:32:22 -0300

    Added attributes

--- OffsitePlayerTest/Global.asax.cs
+++ OffsitePlayerTest/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using NWebsec.Mvc.HttpHeaders.Csp;
 
 namespace OffsitePlayerTest
 {
@@ -11,6 +12,7 @@ namespace OffsitePlayerTest
     {
         protected void Application_Start()
         {
+            GlobalFilters.Filters.Add(new CspAttribute());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
