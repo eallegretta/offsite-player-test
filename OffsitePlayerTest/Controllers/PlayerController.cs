8a554af7a5fcd37d35c002997a9dde1e48f43a9f
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2015-10-14 14:52:45 -0300

    Added project

---
+++ OffsitePlayerTest/Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OffsitePlayerTest.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Home
+        public ActionResult BlogTalkRadio()
+        {
+            return View();
+        }
+
+        public ActionResult Kaltura()
+        {
+            return View();
+        }
+
+        public ActionResult Redirect()
+        {
+            return Redirect("https://securebeta.blogtalkradio.com/offsiteplayer?hostId=38577&episodeId=1504885");
+        }
+    }
+}
--- redirect.aspx
+++
@@ -1,5 +0,0 @@
-<%@ Page Language="C#" %>
-
-<%
-Response.Redirect(Server.MapPath("~/offsite-player3.html"));
-%>
