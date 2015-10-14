7bc57010ccfa98e1637f0f2b2fc0b6baa862e19b
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2015-10-14 17:04:12 -0300

    Added csp

--- OffsitePlayerTest/Controllers/PlayerController.cs
+++ OffsitePlayerTest/Controllers/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NWebsec.Mvc.HttpHeaders.Csp;
 
 namespace OffsitePlayerTest.Controllers
 {
@@ -19,6 +20,10 @@ namespace OffsitePlayerTest.Controllers
             return View();
         }
 
+
+        [CspConnectSrc(CustomSources = "*.facebook.com", Enabled = true)]
+        [CspFrameSrc(CustomSources = "*.facebook.com", Enabled = true)]
+        [CspChildSrc(CustomSources = "*.facebook.com", Enabled = true)]
         public ActionResult Redirect()
         {
             return Redirect("~/offsiteplayer?hostId=38577&episodeId=1504885");
